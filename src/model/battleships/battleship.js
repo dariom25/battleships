878661c4d706041573e00b2d722779769396b2dc
dariom25 <dario@maciey.de>
2024-01-13 20:23:51 +0100

    Implement the ship class with different methods

--- src/model/battleships/battleship.js
+++ src/model/battleships/battleship.js
@@ -1,1 +1,19 @@
 // class has to be exported
+module.exports = class Battleship {
+  constructor(length) {
+    this.length = length;
+    this.hits = 0;
+    this.sunk = false
+  }
+
+  hit() {
+    if (this.length > this.hits) this.hits++;
+  }
+
+  isSunk() {
+    if (this.length === this.hits) {
+        return true;
+    }
+    return false;
+  }
+};
