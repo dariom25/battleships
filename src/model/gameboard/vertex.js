f4bf1fde6c630e89e38fa80944c7995408f2325b
dariom25 <dario@maciey.de>
2024-01-16 19:57:43 +0100

    Add variables for ship and hit

--- src/model/gameboard/vertex.js
+++ src/model/gameboard/vertex.js
@@ -2,6 +2,8 @@ export default class Vertex {
   constructor(coordinates) {
     this.coordinates = coordinates;
     this.connections = [];
+    this.hasShip = false
+    this.isHit = false
   }
 
   addConnection(connection) {
