54947318c548b8481069f1c9d33f620473f4a743
dariom25 <dario@maciey.de>
2024-01-18 18:02:24 +0100

    Make the computerInput method check if the coordinate is legal

--- src/model/player/player.js
+++ src/model/player/player.js
@@ -1,15 +1,30 @@
-export default class Player{
-    
-    playerInput(coordinates) {
-        return coordinates
-    }
+export default class Player {
+  constructor() {
+    this.shots = [];
+  }
+  playerInput(coordinates) {
+    return coordinates;
+  }
 
-    computerInput() {
-        let input = []
-        for (let i = 0; i < 2; i++) {
-            const randomInt = Math.floor(Math.random()*9)
-            input.push(randomInt)
-        }
-        return input
+  computerInput() {
+    let input = [];
+    for (let i = 0; i < 2; i++) {
+      const randomInt = Math.floor(Math.random() * 9);
+      input.push(randomInt);
     }
+    if (this.containsSubarray(this.shots, input)) {
+      this.computerInput()
+    } else {
+      this.shots.push(input);
+      return input;
+    }
+  }
+
+  containsSubarray(outerArray, targetSubarray) {
+    return outerArray.some(
+      (subarray) =>
+        subarray.length === targetSubarray.length &&
+        subarray.every((element, index) => element === targetSubarray[index]),
+    );
+  }
 }
