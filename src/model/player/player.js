5890732ebecf819ecc047c42e652027b5abe1509
dariom25 <dario@maciey.de>
2024-01-18 17:27:50 +0100

    Add method to create random coordinates

--- src/model/player/player.js
+++ src/model/player/player.js
@@ -4,5 +4,12 @@ export default class Player{
         return coordinates
     }
 
-
+    computerInput() {
+        let input = []
+        for (let i = 0; i < 2; i++) {
+            const randomInt = Math.floor(Math.random()*9)
+            input.push(randomInt)
+        }
+        return input
+    }
 }
