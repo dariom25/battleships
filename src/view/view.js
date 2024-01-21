a58cdaf58c38dd54fe4882c21ddba1c8944d7bf7
dariom25 <dario@maciey.de>
2024-01-21 21:08:23 +0100

    Create main content div

--- src/view/view.js
+++ src/view/view.js
@@ -4,6 +4,15 @@ import Header from "./header/header";
 export default class View {
   constructor() {
     this.header = new Header();
-    this.gameboardGrid = new GameboardGrid();
+    this.createMainContent();
+    this.playerGrid = new GameboardGrid();
+    this.computerGrid = new GameboardGrid();
+  }
+
+  createMainContent() {
+    const content = document.querySelector(".content");
+    const mainContent = document.createElement("div");
+    mainContent.classList.add("main-content");
+    content.appendChild(mainContent);
   }
 }
