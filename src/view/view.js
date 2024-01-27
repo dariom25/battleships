0c2fcb42e5a38201bf9d3d1ed1ea9f257668dc69
dariom25 <dario@maciey.de>
2024-01-27 17:44:04 +0100

    Bind event listener to cells

--- src/view/view.js
+++ src/view/view.js
@@ -6,14 +6,27 @@ export default class View {
     this.header = new Header();
     this.playerGrid = new GameboardGrid("Player");
     this.computerGrid = new GameboardGrid("Computer");
+    this.cacheElements();
+    this.bindShootCell();
   }
 
   cacheElements() {
     this.resetButton = document.querySelector(".submit-button");
     this.randomButton = document.querySelector(".random-button");
     this.startButton = document.querySelector(".start-button");
-    this.submitButton = document.querySelector(".submit-button")
-    this.gridContainerPlayer = document.querySelector(".grid-container.Player")
-    this.gridContainerComputer = document.querySelector(".gridContainer.Computer")
+    this.submitButton = document.querySelector(".submit-button");
+    this.gridContainerPlayer = document.querySelector(".grid-container.Player");
+    this.gridContainerComputer = document.querySelector(
+      ".grid-container.Computer",
+    );
+  }
+
+  bindShootCell() {
+    this.gridContainerComputer.addEventListener("click", (event) => {
+      const clickedCell = event.target;
+      if (clickedCell.classList.contains("cell")) {
+        clickedCell.classList.add("shot");
+      }
+    });
   }
 }
