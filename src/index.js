68a0dc744a7684d1f2ed33d240e795489e995ac7
dariom25 <dario@maciey.de>
2024-01-28 19:11:58 +0100

    Clean up code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,1 @@
-import Gameboard from "./model/gameboard/gameboard";
-import Player from "./model/player/player";
-import GameboardGrid from "./view/gameboardComponent/gameboardGrid";
-import "./view/gameboardComponent/grid.css";
-import Header from "./view/header/header";
-import "./view/root.css";
-import "./view/header/header.css"
 import View from "./view/view";
-
-const view = new View();
