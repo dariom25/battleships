5838330209d01dec1029d8da1910d502ec901aa4
dariom25 <dario@maciey.de>
2024-01-30 14:47:48 +0100

    Bind events correctly

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -2,14 +2,15 @@ export default class Controller {
   constructor(model, view) {
     this.model = model;
     this.view = view;
+    this.bindEvents();
   }
 
   bindEvents() {
     this.view.bindShootCell();
-    this.view.bindSubmitCoordinates(this.handleSubmitCoordinates());
+    this.view.bindSubmitCoordinates(this.handleSubmitCoordinates);
   }
 
-  handleSubmitCoordinates() {
-
+  handleSubmitCoordinates = () => {
+    this.view.submitCoordinates();
   }
 }
