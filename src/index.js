8876052b6fb049aee2532f1437dd268d2bee3322
dariom25 <dario@maciey.de>
2024-02-04 19:02:24 +0100

    Add player and computer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import View from "./view/view";
 import Gameboard from "./model/gameboard/gameboard";
 import Controller from "./controller/controller";
+import Player from "./model/player/player";
 
 const view = new View();
 const playerModel = new Gameboard();
 const computerModel = new Gameboard();
-const app = new Controller(playerModel, computerModel, view);
+const player = new Player();
+const computer = new Player();
+const app = new Controller(playerModel, computerModel, view, player, computer);
